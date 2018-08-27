c70da0f2b3f559ab37879076606b4d6c77a480f6
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2018-08-27 09:42:30 +0700

    3.0 and starting with alucard

---
+++ Players/Player.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+export (int) var speed # how fast the player will move (pixels/sec)
+var screensize         # game window size
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2() # The player's movement vector
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	# if Input.is_action_pressed("ui_up"):
+		# nothing
+	# if Input.is_action_pressed("ui_down"):
+		# nothing
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
