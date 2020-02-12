dbb70401a9559a32bd2d2c093e719d5f84ba9a42
gaconkzk <gaconkzk@gmail.com>
2020-02-12 13:59:51 +0700

    get the ball and walk

--- player/kunio/ball.gd
+++ player/kunio/ball.gd
@@ -1,13 +1,10 @@
 extends KinematicBody2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var velocity = Vector2()
-var rotation_dir = 0
-var rotation_speed = 2
-var speed = 100
+var weight = 80
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,24 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #  pass
-
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('ui_right'):
-        velocity.x += 1
-        rotation_dir += 1
-    if Input.is_action_pressed('ui_left'):
-        velocity.x -= 1
-        rotation_dir -= 1
-    if Input.is_action_pressed('ui_down'):
-        velocity.y += 1
-        rotation_dir += 2
-    if Input.is_action_pressed('ui_up'):
-        velocity.y -= 1
-        rotation_dir -= 2
-    velocity = velocity.normalized() * speed
-
-func _physics_process(delta):
-    get_input()
-    rotation = rotation_dir * rotation_speed * delta
-    velocity = move_and_slide(velocity)
--- player/kunio/kunio_base.gd
+++ player/kunio/kunio_base.gd
@@ -1,21 +1,32 @@
 tool
 extends Area2D
 
-
+var speed = 30
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
+var kept_ball
+var kept_ball_dir = 1
 
 func _ready():
-  pass
+#  self.connect("area_entered", self, "hithithit")
+  self.connect("body_entered", self, "meet")
+
+func meet(hit_by):
+  if (hit_by.name == "ball"):
+    kept_ball = hit_by
+    
 
 func flip():
-  if Input.is_action_pressed("ui_left"):
+  if Input.is_action_pressed("ui_left") and not $fanim.flip_h:
     $fanim.flip_h = true
-  if Input.is_action_pressed("ui_right"):
+    kept_ball_dir = -1
+    
+  if Input.is_action_pressed("ui_right") and $fanim.flip_h:
     $fanim.flip_h = false
+    kept_ball_dir = 1
 
 func walk():
   $fanim.play("walk")
@@ -27,11 +38,34 @@ func stop():
   $fanim.stop()
   $fanim.frame = 0
 
+func move(delta):
+  var velocity = Vector2()
+  var dist = speed*delta
+  if Input.is_action_pressed('ui_right'):
+      velocity.x += dist
+  if Input.is_action_pressed('ui_left'):
+      velocity.x -= dist
+  if Input.is_action_pressed('ui_down'):
+      velocity.y += dist
+      velocity.x = velocity.x * 0.87
+  if Input.is_action_pressed('ui_up'):
+      velocity.y -= dist
+      velocity.x = velocity.x * 0.87
+  return velocity
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
   var action_pressed = Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_down") || Input.is_action_pressed("ui_up")
   if action_pressed:
     flip()
     walk()
   else:
     stop()
+
+func _physics_process(delta):
+  var velo = move(delta)
+  position += velo
+  if kept_ball:
+    kept_ball.position = position
+    kept_ball.position.x += 10*kept_ball_dir
+    kept_ball.position.y += 1
